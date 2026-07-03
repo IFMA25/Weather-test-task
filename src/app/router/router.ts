554a77ad357f2dab37a768419d98cd0cf70c7887
IFMA25 <innamassura@gmail.com>
2026-07-03 21:08:13 +0300

    fix: router

--- src/app/router/router.ts
+++ src/app/router/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHashHistory, type RouteRecordRaw } from "vue-router";
 
 import FavoritesPage from "@/pages/FavoritesPage.vue";
 import NotFoundPage from "@/pages/NotFoundPage.vue";
@@ -30,7 +30,7 @@ const routes: RouteRecordRaw[] = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
 
